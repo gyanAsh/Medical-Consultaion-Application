a12e7dac49fd9363973dfc076958db984b56a74c
gyanAsh <gyanash36@protonmail.com>
2022-05-18 21:37:47 +0530

    Added Appointment and DoctorList Component as tabs in the Home Component" git push "

---
+++ BookMyConsultationUI/src/screens/doctorList/DoctorList.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const DoctorList = () => {
+    return (
+        <div>
+            Doctor
+        </div>
+    )
+}
+
+export default DoctorList;
--- BookMyConsultationUI/src/screens/home/Home.js
+++ BookMyConsultationUI/src/screens/home/Home.js
@@ -1,13 +1,76 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from '../../common/header/Header';
+import DoctorList from '../../screens/doctorList/DoctorList';
+import Appointment from '../../screens/appointment/Appointment';
+import PropTypes from 'prop-types'
+import {Tabs,Tab,Box,Typography} from '@material-ui/core';
 
 const Home = () => {
     return (
         <div>
-            {/* temptory */}
-            <Header/>
+            <Header />
+            <BasicTabs/>
         </div>
     )
 }
 
 export default Home;
+
+
+const TabPanel=props=> {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3  }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+  
+  export function BasicTabs() {
+    const [value, setValue] = useState(0);
+  
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+  
+    return (
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs  value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="DOCTORS" {...a11yProps(0)} />
+            <Tab label="APPOINTMENT" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <DoctorList/>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Appointment/>
+        </TabPanel>
+      </Box>
+    );
+  }
